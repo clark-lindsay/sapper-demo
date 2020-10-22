3fd7ba9ac96ffe28e3334e06c5ea67cf706d2204
clark-lindsay <clarklindsay96@gmail.com>
2020-10-22 16:04:19 -0400

    Add some fairolina sample material

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <script>
-  import successkid from "images/successkid.jpg";
+  import fairolinaLogoFull from "../../static/FAIR1.jpg";
   import { onMount } from "svelte";
 
   onMount(() => {
@@ -16,53 +16,40 @@
 </script>
 
 <style>
-  h1,
   figure,
-  p {
-    text-align: center;
-    margin: 0 auto;
-  }
-
-  h1 {
-    font-size: 2.8em;
-    text-transform: uppercase;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
-  }
-
   figure {
-    margin: 0 0 1em 0;
+    margin: 1em;
   }
 
   img {
     width: 100%;
     max-width: 400px;
-    margin: 0 0 1em 0;
-  }
-
-  p {
-    margin: 1em auto;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
-    }
+    margin: 1em;
   }
 </style>
 
 <svelte:head>
-  <title>Sapper project template</title>
+  <title>Fairolina Demo</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<figure><img alt="FAIROLINA" src={fairolinaLogoFull} /></figure>
 
-<figure>
-  <img alt="Success Kid" src={successkid} />
-  <figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
+<h1>Welcome to FAIRolina!</h1>
 <p>
-  <strong>Try editing this file (src/routes/index.svelte) to test live
-    reloading.</strong>
+  We are an organization for students at the University of North Carolina at
+  Chapel Hill who are concerned about the democratic institutions of our state
+  and our nation. We believe that reform is required in order to ensure the
+  fairness and and representative legitimacy of our political system, and that,
+  in the absence of this reform, the integrity of our democratic institutions is
+  decaying at a dangerous pace. We also believe that a lack of fairness in our
+  election systems is at the root of much of the gridlock and partisan
+  polarization we see in Raleigh and in Washington. When politicians do not feel
+  accountable to voters, they are able to act without considering the needs or
+  interests of their constituents. Gerrymandering, registration purges, lack of
+  access to polling sites, voter ID laws, poorly defined oversight, corporate
+  money in politics, the Electoral College, felon disenfranchisement, and
+  foreign interference are just some of the ways the voices American voters are
+  diminished or ignored. See our policy page for a specific collection of
+  changes we support in order to modernize and improve our democracy!
 </p>
+
